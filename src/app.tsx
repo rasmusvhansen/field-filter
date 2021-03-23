0f52d91b922cf7cf75ce0f6cfbe9966517f362c6
Rasmus Vestergaard Hansen <rasmusvhansen@gmail.com>
2021-03-23 23:10:47 +0100

    fix: dynamic creation of fields and areas

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'preact/hooks';
 import useLocalStorage from './hooks/useLocalStorage';
 import { ExcelUpload } from './ExcelUpload';
 import './app.css';
+const DEFAULT_COLOR = '#0000ff';
 
 function without<T>(rec: Record<string, T>, id: string): Record<string, T> {
   const { [id]: value, ...rest } = rec;
@@ -13,21 +14,36 @@ export function App() {
   const path = location.pathname.slice(1);
   const getId = (id: string) => `${path}-${id}`;
   const [img, setImg] = useState('');
+  const [fieldName, setFieldName] = useState('');
+  const [areaName, setAreaName] = useState('');
   const [track, setTrack] = useState<CoordWithTime[]>([]);
-  const [areaIds, setAreaIds] = useState<string[]>(['a1', 'a2', 'a3']);
-  const [fieldIds, setFieldIds] = useState<string[]>(['f1', 'f2', 'f3']);
   const [areas, setAreas] = useLocalStorage<Record<string, Area>>(getId('areas'), {});
   const [fields, setFields] = useLocalStorage<Record<string, Area>>(getId('fields'), {});
   const removeField = (id: string) => {
     setFields(without(fields, id));
-    setFieldIds(fieldIds.filter((i) => i !== id));
   };
   const removeArea = (id: string) => {
     setAreas(without(areas, id));
-    setAreaIds(areaIds.filter((i) => i !== id));
+  };
+  const createField = () => {
+    if (fieldName) {
+      setFields({ ...fields, [fieldName]: { id: fieldName, coords: [], color: DEFAULT_COLOR } });
+      setFieldName('');
+    }
+  };
+
+  const createArea = () => {
+    if (areaName) {
+      setAreas({ ...areas, [areaName]: { id: areaName, coords: [], color: DEFAULT_COLOR } });
+      setAreaName('');
+    }
   };
   useEffect(() => {
-    getMapUrl(Object.values(areas).concat(Object.values(fields))).then((url) => setImg(url));
+    getMapUrl(
+      Object.values(areas)
+        .filter((a) => a.coords.length)
+        .concat(Object.values(fields).filter((a) => a.coords.length))
+    ).then((url) => setImg(url));
   }, [areas, fields]);
   return (
     <>
@@ -39,21 +55,29 @@ export function App() {
       <div class="area-wrapper">
         <div>
           <h3>Marker</h3>
-          {fieldIds.map((id) => (
+          <div>
+            <input type="text" value={fieldName} onChange={(e) => setFieldName(e.currentTarget.value)} />
+            <button onClick={createField}>Opret Mark</button>
+          </div>
+          {Object.values(fields).map((field) => (
             <Polygon
-              key={id}
-              area={fields[id] || { id, coords: [], color: '#0000ff' }}
-              onUpload={(area) => setFields({ ...areas, [area.id]: area })}
+              key={field.id}
+              area={field}
+              onUpload={(area) => setFields({ ...fields, [area.id]: area })}
               onDelete={removeField}
             ></Polygon>
           ))}
         </div>
         <div class="areas">
           <h3>Skovområder</h3>
-          {areaIds.map((id) => (
+          <div>
+            <input type="text" onChange={(e) => setAreaName(e.currentTarget.value)} />
+            <button onClick={createArea}>Opret Skovområde</button>
+          </div>
+          {Object.values(areas).map((area) => (
             <Polygon
-              key={id}
-              area={areas[id] || { id, coords: [], color: '#0000ff' }}
+              key={area.id}
+              area={area}
               onUpload={(area) => setAreas({ ...areas, [area.id]: area })}
               onDelete={removeArea}
             ></Polygon>
