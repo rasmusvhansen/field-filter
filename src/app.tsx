f229937f8291201e86082ba039954f3b1b605df1
Rasmus Vestergaard Hansen <rasmusvhansen@gmail.com>
2021-03-23 22:49:26 +0100

    fix: add dynamic areas

--- src/app.tsx
+++ src/app.tsx
@@ -2,23 +2,33 @@ import { Area, Coord, CoordWithTime, getMapUrl, parsePositionFile, pointInPoly }
 import { useEffect, useState } from 'preact/hooks';
 import useLocalStorage from './hooks/useLocalStorage';
 import { ExcelUpload } from './ExcelUpload';
-const brabrand: Coord[] = [
-  [56.16087927816185, 10.105038391804111],
-  [56.16028182448348, 10.11027406337594],
-  [56.159158586411586, 10.109372841220132],
-  [56.15920638444541, 10.107784973612285],
-  [56.15825041246916, 10.107355820204756],
-  [56.15860890474803, 10.104351746352068],
-];
+import './app.css';
+
+function without<T>(rec: Record<string, T>, id: string): Record<string, T> {
+  const { [id]: value, ...rest } = rec;
+  return rest;
+}
 
 export function App() {
+  const path = location.pathname.slice(1);
+  const getId = (id: string) => `${path}-${id}`;
   const [img, setImg] = useState('');
   const [track, setTrack] = useState<CoordWithTime[]>([]);
-  const [areas, setAreas] = useState<Record<string, Area>>({});
-  const [fields, setFields] = useState<Record<string, Area>>({});
+  const [areaIds, setAreaIds] = useState<string[]>(['a1', 'a2', 'a3']);
+  const [fieldIds, setFieldIds] = useState<string[]>(['f1', 'f2', 'f3']);
+  const [areas, setAreas] = useLocalStorage<Record<string, Area>>(getId('areas'), {});
+  const [fields, setFields] = useLocalStorage<Record<string, Area>>(getId('fields'), {});
+  const removeField = (id: string) => {
+    setFields(without(fields, id));
+    setFieldIds(fieldIds.filter((i) => i !== id));
+  };
+  const removeArea = (id: string) => {
+    setAreas(without(areas, id));
+    setAreaIds(areaIds.filter((i) => i !== id));
+  };
   useEffect(() => {
-    getMapUrl(Object.values(areas)).then((url) => setImg(url));
-  }, [areas]);
+    getMapUrl(Object.values(areas).concat(Object.values(fields))).then((url) => setImg(url));
+  }, [areas, fields]);
   return (
     <>
       <ExcelUpload
@@ -26,49 +36,76 @@ export function App() {
         fields={Object.values(fields)}
         areas={Object.values(areas)}
       ></ExcelUpload>
-      <FileUpload id="Mark1" handleUpload={(area) => setAreas((prev) => ({ ...prev, [area.id]: area }))}></FileUpload>
-      <FileUpload id="Mark2" handleUpload={(area) => setAreas((prev) => ({ ...prev, [area.id]: area }))}></FileUpload>
+      <div class="area-wrapper">
+        <div>
+          <h3>Marker</h3>
+          {fieldIds.map((id) => (
+            <Polygon
+              key={id}
+              area={fields[id] || { id, coords: [], color: '#0000ff' }}
+              onUpload={(area) => setFields({ ...areas, [area.id]: area })}
+              onDelete={removeField}
+            ></Polygon>
+          ))}
+        </div>
+        <div class="areas">
+          <h3>Skovområder</h3>
+          {areaIds.map((id) => (
+            <Polygon
+              key={id}
+              area={areas[id] || { id, coords: [], color: '#0000ff' }}
+              onUpload={(area) => setAreas({ ...areas, [area.id]: area })}
+              onDelete={removeArea}
+            ></Polygon>
+          ))}
+        </div>
+      </div>
+
       <div>
         <img src={img} alt="" />
       </div>
-      <p>Indenfor {pointInPoly(brabrand, [56.15976869272208, 10.107111589895874])}</p>
-      <p>Indenfor {pointInPoly(brabrand, [56.16014891606887, 10.109797186864864])}</p>
-      <p>Indenfor {pointInPoly(brabrand, [56.1588392420307, 10.10513911754577])}</p>
-      <p>Udenfor {pointInPoly(brabrand, [56.15873784604939, 10.108431629084023])}</p>
-      <p>Udenfor {pointInPoly(brabrand, [56.160757265595734, 10.10148245726596])}</p>
-      <p>Udenfor {pointInPoly(brabrand, [56.15813791435047, 10.106944688389326])}</p>
     </>
   );
 }
 
-function FileUpload(props: { id: string; handleUpload: (area: Area) => void }) {
-  const [area, setArea] = useLocalStorage<Area | null>(props.id, null);
+function Polygon({
+  area,
+  onUpload,
+  onDelete,
+}: {
+  area: Area;
+  onUpload: (area: Area) => void;
+  onDelete: (id: string) => void;
+}) {
   const [color, setColor] = useState(area?.color || '#0000ff');
+  const [newArea, setNewArea] = useState<Area | null>(null);
   useEffect(() => {
-    if (area) {
-      setArea({ ...area, color });
+    const a = newArea ? newArea : area.coords.length ? area : null;
+    if (a) {
+      setNewArea({ ...a, color });
     }
   }, [color]);
   useEffect(() => {
-    if (area) {
-      props.handleUpload(area);
+    if (newArea) {
+      onUpload(newArea);
     }
-  }, [area]);
+  }, [newArea]);
   const loadFile = (e: any) => {
     console.log(e.target!.files[0]);
     const reader = new FileReader();
     reader.onload = (ev) => {
       const text = ev.target?.result as string;
       if (text) {
-        setArea({ id: props.id, coords: parsePositionFile(text)[0], color });
+        setNewArea({ id: area.id, coords: parsePositionFile(text)[0], color });
       }
     };
     reader.readAsText(e.target!.files[0]);
   };
   return (
     <div>
-      {props.id} <input type="file" name="upload" id="" onChange={(e) => loadFile(e)} />{' '}
+      {area.id} <input type="file" name={area.id} id={area.id} onChange={(e) => loadFile(e)} />{' '}
       <input type="color" value={color} onChange={(e: any) => setColor(e.target.value)} />
+      <button onClick={() => onDelete(area.id)}>Slet</button>
     </div>
   );
 }
